d977c774a8fa985bfe47e93fbc113fc0397a82e8
grouls <roulstone.gary@gmail.com>
2024-02-19 16:03:21 +0000

    Take user input and write to a file

---
+++ app.js
@@ -0,0 +1,50 @@
+const http = require("http");
+const fs = require("fs");
+
+const server = http.createServer((req, res) => {
+  const url = req.url;
+  const method = req.method;
+
+  if (url === "/") {
+    res.setHeader("Content-Type", "text/html");
+    res.write("<html>");
+    res.write("<head><title>Enter Message</title><head>");
+    res.write(`
+        <body>
+            <form action="/message" method="POST">
+                <input type="text" name="message">
+                <button type="submit">Send</button>
+            </form>
+        </body>
+    `);
+    res.write("</html>");
+    return res.end();
+  }
+
+  if (url === "/message" && method === "POST") {
+    const body = [];
+    req.on("data", (chunk) => {
+      body.push(chunk);
+    });
+
+    req.on("end", () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split("=")[1];
+      fs.writeFileSync("message.txt", message);
+    });
+    res.statusCode = 302;
+    res.setHeader("Location", "/");
+    return res.end();
+  }
+  res.setHeader("Content-Type", "text/html");
+  res.write("<html>");
+  res.write("<head><title>Message Page</title><head>");
+  res.write(`
+        <body>
+            Hello from my Node.js server!
+        </body>
+    `);
+  res.write("</html>");
+});
+
+server.listen(3000);
