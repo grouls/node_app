8e90503057dd7a7acc2229dc757f7b02b5567fc0
grouls <roulstone.gary@gmail.com>
2024-02-19 21:15:13 +0000

    write usernames to a file

--- routes.js
+++ routes.js
@@ -7,19 +7,35 @@ const requestHandler = (req, res) => {
     res.setHeader("Content-Type", "text/html");
     res.write(`
         <html>
-            <head><title>Enter Message</title><head>
+            <head><title>Homepage</title><head>
             <body>
-                <form action="/message" method="POST">
-                    <input type="text" name="message">
-                    <button type="submit">Send</button>
+                <h1>Welcome to my node.js app</h1>
+                <form action="/create-user" method="POST">
+                    <input type="text" name="username">
+                    <button type="Submit">Add User</button>
                 </form>
             </body>
         </html>
     `);
     return res.end();
   }
+  if (url === "/users") {
+    res.setHeader("Content-Type", "text/html");
+    res.write(`
+        <html>
+            <head><title>Add User</title><head>
+            <body>
+                <ul>
+                    <li>User 1</li>
+                    <li>User 2</li>
+                </ul>
+            </body>
+        </html>
+    `);
+    return res.end();
+  }
 
-  if (url === "/message" && method === "POST") {
+  if (url === "/create-user" && method === "POST") {
     const body = [];
     req.on("data", (chunk) => {
       body.push(chunk);
@@ -28,22 +44,24 @@ const requestHandler = (req, res) => {
     return req.on("end", () => {
       const parsedBody = Buffer.concat(body).toString();
       const message = parsedBody.split("=")[1];
-      fs.writeFile("message.txt", message, (err) => {
+      fs.writeFile("users.txt", message, (err) => {
         res.statusCode = 302;
         res.setHeader("Location", "/");
         return res.end();
       });
     });
   }
+
   res.setHeader("Content-Type", "text/html");
   res.write(`
-        <html>
-            <head><title>Message Page</title><head>
-            <body>
-                <h1>Hello from my Node.js server!</h1>
-            </body>
-        </html>
-    `);
+      <html>
+          <head><title>Something went wrong</title><head>
+          <body>
+              <h1>Page not found</h1>
+          </body>
+      </html>
+  `);
+  return res.end();
 };
 
 module.exports = { handler: requestHandler };
