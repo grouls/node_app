ea5f9fcf0161a896b3d547816ebff50ba697281b
grouls <roulstone.gary@gmail.com>
2024-02-29 15:19:57 +0000

    add more routes and view details functionality

--- models/product.js
+++ models/product.js
@@ -18,22 +18,32 @@ const getProductsFromFile = (callback) => {
 };
 
 module.exports = class Product {
-  constructor(product) {
-    this.title = product.title;
-    this.price = "€" + product.price;
-    this.description = product.description;
+  constructor(title, imageUrl, description, price) {
+    this.title = title;
+    this.imageUrl = imageUrl;
+    this.description = description;
+    this.price = price;
   }
 
   save() {
+    // Not actually unique but will do for now
+    this.id = Math.random().toString();
     getProductsFromFile((products) => {
       products.push(this);
       fs.writeFile(p, JSON.stringify(products), (err) => {
-        console.log("🚀 ~ Product ~ fs.writeFile ~err:", err);
+        err && console.log("🚀 ~ Product ~ fs.writeFile ~err:", err);
       });
     });
   }
 
   static fetchAll(callback) {
     getProductsFromFile(callback);
+  }
+
+  static findById(id, callback) {
+    getProductsFromFile((products) => {
+      const product = products.find((p) => p.id === id);
+      callback(product);
+    });
   }
 };
