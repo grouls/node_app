0a25d1fbe2314bd78b1e8109623b048a6c77ee27
grouls <roulstone.gary@gmail.com>
2024-03-01 13:15:20 +0000

    hook up add to cart placeholder page

--- controllers/shop.js
+++ controllers/shop.js
@@ -38,6 +38,12 @@ exports.getCart = (req, res, next) => {
   res.render("shop/cart", { path: "/cart", pageTitle: "Your Cart" });
 };
 
+exports.postCart = (req, res, next) => {
+  const productId = req.body.productId;
+  console.log("🚀 ~ productId:", productId);
+  res.redirect("cart");
+};
+
 exports.getOrders = (req, res, next) => {
   res.render("shop/orders", { path: "/orders", pageTitle: "Your Orders" });
 };
--- routes/shop.js
+++ routes/shop.js
@@ -5,10 +5,17 @@ const shopController = require("../controllers/shop");
 const router = express.Router();
 
 router.get("/", shopController.getIndex);
+
 router.get("/products", shopController.getProducts);
+
 router.get("/product-detail/:productId", shopController.getProduct);
+
 router.get("/cart", shopController.getCart);
+
+router.post("/cart", shopController.postCart);
+
 router.get("/orders", shopController.getOrders);
+
 router.get("/checkout", shopController.getCheckout);
 
 module.exports = router;
